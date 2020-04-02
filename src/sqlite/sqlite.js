554e28277dac105cbbf5e514e1e1ca1afe0f99d4
Beng Tan <bengtan@users.noreply.github.com>
2020-04-02 12:43:09 +0800

    Tighten sqlite.js.

--- src/sqlite/sqlite.js
+++ src/sqlite/sqlite.js
@@ -9,26 +9,16 @@ sqlite.Database.prototype.open = function() {
     return new Promise((resolve, reject) => {
         _sqliteMux('open', this.filename).then((id) => {
             this._id = id
-            resolve(this)
-        }).catch((e) => {
-            reject(e)
-        })
+            resolve()
+        }).catch(e => reject(e))
     })
 }
 
 sqlite.Database.prototype.close = function() {
     return new Promise((resolve, reject) => {
         const handle = this._id
-        if (handle >= 0) {
-            this._id = -1
-            _sqliteMux('close', handle).then(() => {
-                resolve(this)
-            }).catch((e) => {
-                reject(e)
-            })
-        }
-        else {
-            reject(`Already closed`)
-        }
+        this._id = -1
+        _sqliteMux('close', handle)
+        .then(() => resolve()).catch(e => reject(e))
     })
 }
